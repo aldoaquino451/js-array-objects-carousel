4ef44c090e8d1325f6735021fa66d7b21e2ce140
aldoaquino451 <aldoaquino2909@gmail.com>
2023-09-22 17:55:37 +0200

    [add] database delle immagini

--- js/script.js
+++ js/script.js
@@ -7,45 +7,48 @@
 
 const images = [
   {
-      image: 'img/01.webp',
-      title: 'Marvel\'s Spiderman Miles Morale',
-      text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+      image: './img/cyberpunk.jpg',
+      title: 'Cyberpunk 2077',
+      text: 'Cyberpunk 2077 è un videogioco sparatutto in prima persona di genere action RPG e open world, sviluppato da CD Projekt RED e pubblicato da CD Projekt',
   }, 
   {
-      image: 'img/02.webp',
-      title: 'Ratchet & Clank: Rift Apart',
-      text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+      image: './img/elder-ring.webp',
+      title: 'Elder Ring',
+      text: 'E\' un videogioco action RPG sviluppato dalla software house giapponese FromSoftware e pubblicato da Bandai Namco Entertainment. Il videogioco, diretto da Hidetaka Miyazaki, è stato realizzato in collaborazione con George R. R. Martin, autore statunitense di fantasy noto soprattutto per il ciclo di romanzi Cronache del ghiaccio e del fuoco.',
   }, 
   {
-      image: 'img/03.webp',
-      title: 'Fortnite',
-      text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+      image: './img/zelda.jpg',
+      title: 'The Legend of Zelda: Breath of the Wild',
+      text: 'E\' un videogioco action-adventure del 2017, sviluppato da Nintendo EPD e pubblicato da Nintendo per Nintendo Switch e Wii U. Si tratta del diciottesimo capitolo della celebre saga di videogiochi The Legend of Zelda.',
   }, 
   {
-      image: 'img/04.webp',
-      title: 'Stray',
-      text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-  }, 
+      image: './img/last-of-us.jpg',
+      title: "The Last of Us Parte II",
+      text: 'E\' un videogioco di avventura dinamica, a sfondo post-apocalittico, sviluppato dalla casa di produzione Naughty Dog in esclusiva per PlayStation 4. È il sequel di The Last of Us e si svolge cinque anni dopo quest\'ultimo.',
+  },
   {
-      image: 'img/05.webp',
-      title: "Marvel's Avengers",
-      text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+    image: './img/portal.jpg',
+    title: 'Portal 2',
+    text: 'Portal 2 è un videogioco rompicapo, pubblicato nel 2011 da Valve Corporation per varie piattaforme.',
   }
 ];
 
+const counterMax = images.length;
+
+/*
 const source = [
   './img/cyberpunk.jpg',
   './img/elder-ring.webp',
-  './img/zelda.jpg',
+  './img/./img/zelda.jpg',
   './img/last-of-us.jpg',
   './img/portal.jpg',
 ]
+*/
 
 const imagesBox = document.querySelector('.carousel-image');
 const thumbsBox = document.querySelector('.carousel-thumb');
 const btnUp = document.querySelector('.arrow-btn.up');
 const btnDown = document.querySelector('.arrow-btn.down');
-const counterMax = source.length;
 
 let counter;
 
@@ -55,12 +58,22 @@ thumbsBox.innerHTML = '';
 
 /* ------- IMAGES & THUMBS -------- */
 
-for ( let i = 0; i < counterMax; i ++ ) {
-  
-  imagesBox.innerHTML += `<img class="item hide" src="${source[i]}" alt="01">`;
-  thumbsBox.innerHTML += `<img class="mini-item" src="${source[i]}" alt="01">`
-  
-};
+images.forEach( copertina => {
+
+  imagesBox.innerHTML += `
+  <div class="hide item">
+    <img src="${copertina.image}" alt="cyberpunk">
+    <div class="text">
+      <h2>${copertina.title}</h2>
+      <p>${copertina.text}</p>
+    </div>
+  </div>
+  `;
+
+  thumbsBox.innerHTML += `<img class="mini-item" src="${copertina.image}" alt="01">`
+
+});
+
 
 const imagesArr = document.getElementsByClassName('item');
 const thumbsArr = document.getElementsByClassName('mini-item');
